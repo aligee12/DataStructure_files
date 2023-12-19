00e6da973b4c27a7523b1dcc8a1ee9b2ac80774f
aligee12 <itzsyed1212@gmail.com>
2023-12-19 22:21:24 +0500

    corrected code of sorting

--- bubbleSort.cpp
+++ bubbleSort.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 void bubbleSort(int arr[],int n){
     for(int i=1; i<n; i++){
-        for(int j=0; j<n; j++){
+        for(int j=0; j<n-i-1; j++){
             if(arr[j] > arr[j+1]){
                 int temp = arr[j];
                 arr[j] = arr[j+1];
