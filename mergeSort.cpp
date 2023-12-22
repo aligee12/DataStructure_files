eb7453791615d3975e37f4c1140371da91e279f4
aligee12 <itzsyed1212@gmail.com>
2023-12-22 10:44:14 +0500

    Merge Sort Completed

--- mergeSort.cpp
+++ mergeSort.cpp
@@ -2,6 +2,39 @@
 using namespace std;
 
 void merge(int arr[], int l, int mid, int r){
+    int n1 = mid-l+1;       //mid included
+    int n2 = r-mid;         //after mid
+
+    int a[n1];
+    int b[n2];
+
+    for (int i = 0; i< n1; i++){        //mid is added in a
+        a[i] = arr[l+i];
+    }
+    for (int i = 0; i< n2; i++){        //b starts after mid
+        b[i] = arr[mid+i+1];
+    }
+
+    int i =0;
+    int j = 0;
+    int k = l;
+    while (i<n1 && j < n2){
+        if (a[i] < b[j]){
+            arr[k] = a[i];
+            k++; i++;
+        }else{
+            arr[k] = b[j];
+            k++; j++;
+        }
+    }
+    while(i<n1){
+        arr[k] = a[i];
+        k++; i++;
+    }
+    while(j< n2){
+        arr[k] = b[j];
+            k++; j++;
+    }
 
 }
 void mergeSort(int arr[],int l,int r){
@@ -13,6 +46,13 @@ void mergeSort(int arr[],int l,int r){
         merge(arr,l,mid,r);
     }
 }
-void main(){
+int main(){
+    int array[] = {5,4,3,2,1};    
+    mergeSort(array,0,4);
+    cout << "\nSorted array elements: ";
+    for(int i =0; i<5; i++){        
+        cout << array[i] << " ";
+    }
+    cout << endl;
     return 0;
 }
